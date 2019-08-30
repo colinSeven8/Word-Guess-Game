3605ab538b5b59eccd190d04a59ed653cd82426d
colinSeven8 <ccnorse80@gmail.com>
2019-08-30 15:57:12 -0600

    Wireframing the html, adding body to css and beginning js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,16 @@
-const wordArr = ["Ollie", "Shuvit", "Halfcab", "Kickflip", "Heelflip", "Nollie", "Impossible", "Threeflip", "Darkslide", "Boneless", "Nocomply"];
+$(document).ready(function () {
 
-var randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+    const wordArr = ["Ollie", "Shuvit", "Halfcab", "Kickflip", "Heelflip", "Nollie", "Impossible", "Threeflip", "Darkslide", "Boneless", "Nocomply"];
 
-var dashArr = [];
+    var randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];
 
-function onStartUp() {
+    var dashArr = [];
 
-    wordArr.forEach(trick => {
-        dashArr.push("_");
-    });
-
-}
-
-let wordGame = {
-    wordArr: ["Ollie", "Shuvit", "Halfcab", "Kickflip", "Heelflip", "Nollie", "Impossible", "Threeflip", "Darkslide", "Boneless", "Nocomply"],
-    randomWord: wordArr[Math.floor(Math.random() * wordArr.length)],
-    dashArr: [],
-
+    //Load the dash array with the appropriate number of dashes
     function onStartUp() {
-
         wordArr.forEach(trick => {
             dashArr.push("_");
         });
-    
     }
-};
+
+}
