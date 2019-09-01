103fe2831e574ef14213a15e537454d3fbe38cf9
colinSeven8 <ccnorse80@gmail.com>
2019-09-01 15:36:29 -0600

    Added some objects to css, some audio, some images.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,73 @@
-$(document).ready(function () {
+// Colin Reesor
+// Word Guess Game
+// game.js
 
-    const wordArr = ["Ollie", "Shuvit", "Halfcab", "Kickflip", "Heelflip", "Nollie", "Impossible", "Threeflip", "Darkslide", "Boneless", "Nocomply"];
+//Auxiliary Objects
+const Initial= {
+    "Text" : "Can you guess that trick?",
+    "Picture" : "assets/images/skater.png",
+    "Sound" : "assets/audio/imnotaloser.mp3"
+};
 
-    var randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+const Victory = {
+    "Text" : "You got it!",
+    "Picture" : "assets/images/victory.jpg",
+    "Sound" : "assets/audio/karatekid.mp3"
+};
 
-    var dashArr = [];
+const GameOver = {
+    "Text" : "Game Over!",
+    "Picture" : "assets/images/crash.jpg",
+    "Sound" : "assets/audio/laugh.mp3"
+};
 
-    //Load the dash array with the appropriate number of dashes
-    function onStartUp() {
-        wordArr.forEach(trick => {
-            dashArr.push("_");
-        });
-    }
+//Movie Clip Objects
+const Ollie = {
+    "trickName" : "ollie",
+    "trickText" : "Yup!",
+    "trickVideo" : "#",
+};
 
-}
+const Shuvit = {
+    "trickName" : "shuvit",
+    "trickText" : "Will ya look at that!",
+    "trickVideo" : "#",
+};
+
+const Kickflip = {
+    "trickName" : "kickflip",
+    "trickText" : "Stuff is happening!",
+    "trickVideo" : "#",
+};
+
+const Heelflip = {
+    "trickName" : "heelflip",
+    "trickText" : "Well done, son!",
+    "trickVideo" : "#",
+};
+
+const Nollie = {
+    "trickName" : "nollie",
+    "trickText" : "Yup again!",
+    "trickVideo" : "#",
+};
+
+const Boneless = {
+    "trickName" : "boneless",
+    "trickText" : "You are certainly special!",
+    "trickVideo" : "#",
+};
+
+const Threeflip = {
+    "trickName" : "threeflip",
+    "trickText" : "Whirly-whoopty-woo!",
+    "trickVideo" : "#",
+};
+
+const Impossible = {
+    "trickName" : "impossible",
+    "trickText" : "Lordy, the possiblities!",
+    "trickVideo" : "#",
+};
+
+//Ollie, Shuvit, Kickflip, Heelflip, Nollie, Boneless, Threeflip, Impossible
