ca14096d06b88cb1bc0e0324f1ec79155855cafc
colinSeven8 <ccnorse80@gmail.com>
2020-05-11 08:42:08 -0600

    Updated DOM manipulation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,89 +2,76 @@
 // Word Guess Game
 // game.js
 
-//Auxiliary obect array - Initial, Win, Loss.
+//Auxiliary object array - Initial, Win, Loss.
 let aux = [
-    {
-        "name": "initial",
-        "text": "Can you guess that trick?",
-        "image": "assets/images/skater.png",
-        "audio": "assets/audio/imnotaloser.mp3"
-    },
-
-    {
-        "name": "win",
-        "text": "You got it!",
-        "image": "assets/images/victory.jpg",
-        "audio": "assets/audio/karatekid.mp3"
-    },
-
-    {
-        "name": "loss",
-        "text": "Game Over!",
-        "image": "assets/images/crash.jpg",
-        "audio": "assets/audio/laugh.mp3"
-    }
+  {
+    text: "Can you guess that trick?",
+    msg: "Good luck!",
+    image: "assets/images/skater.png",
+    audio: "assets/audio/imnotaloser.mp3",
+  },
+  {
+    text: "You got it!",
+    image: "assets/images/victory.jpg",
+  },
+  {
+    text: "Game Over!",
+    image: "assets/images/crash.jpg",
+  },
 ];
 
 //Skate trick object array - Ollie, Shuvit, Kickflip, Heelflip, Nollie, Boneless, Threeflip, Impossible.
-let words = [
-    {
-        "trickName": "ollie",
-        "trickText": "Yup!",
-        "trickVideo": "#"
-    },
-
-    {
-        "trickName": "shuvit",
-        "trickText": "Will ya look at that!",
-        "trickVideo": "#"
-    },
-
-    {
-        "trickName": "kickflip",
-        "trickText": "Stuff is happening!",
-        "trickVideo": "#"
-    },
-
-    {
-        "trickName": "heelflip",
-        "trickText": "Well done, son!",
-        "trickVideo": "#"
-    },
-
-    {
-        "trickName": "nollie",
-        "trickText": "Yup again!",
-        "trickVideo": "#"
-    },
-
-    {
-        "trickName": "boneless",
-        "trickText": "You are certainly special!",
-        "trickVideo": "#"
-    },
-
-    {
-        "trickName": "threeflip",
-        "trickText": "Whirly-whoopty-woo!",
-        "trickVideo": "#"
-    },
-
-    {
-        "trickName": "impossible",
-        "trickText": "Lordy, the possiblities!",
-        "trickVideo": "#"
-    }
+let tricks = [
+  {
+    trickName: "ollie",
+    trickText: "Yup!",
+    trickVideo: "https://media.giphy.com/media/1BgqELRJaEQqufiGmz/giphy.gif",
+  },
+  {
+    trickName: "shuvit",
+    trickText: "Will ya look at that!",
+    trickVideo: "https://media.giphy.com/media/YVYWXf3bHcNnQHRPUU/giphy.gif",
+  },
+  {
+    trickName: "kickflip",
+    trickText: "Stuff is happening!",
+    trickVideo: "https://media.giphy.com/media/5UxHLH17XsFm8iJesT/giphy.gif",
+  },
+  {
+    trickName: "heelflip",
+    trickText: "Well done, son!",
+    trickVideo: "https://media.giphy.com/media/i4bTcbeNVI8evqzLS5/giphy.gif",
+  },
+  {
+    trickName: "nollie",
+    trickText: "Yup again!",
+    trickVideo: "https://media.giphy.com/media/8FM8xhCSZjKyafqZy3/giphy.gif",
+  },
+  {
+    trickName: "boneless",
+    trickText: "You are certainly special!",
+    trickVideo: "https://media.giphy.com/media/kyWekBsvsWLz0BMkko/giphy.gif",
+  },
+  {
+    trickName: "threeflip",
+    trickText: "Whirly-whoopty-woo!",
+    trickVideo: "https://media.giphy.com/media/GYt8vu7nSsdLa/giphy.gif",
+  },
+  {
+    trickName: "impossible",
+    trickText: "Lordy, the possiblities!",
+    trickVideo: "https://media.giphy.com/media/l0IulSRkjIYAaCHCM/giphy.gif",
+  },
 ];
 
 //Set up globals.
-let wordsRandomOrder = randomOrder(words);
+let tricksRandomOrder = randomOrder(tricks);
 let currentIndex = 0;
-let currentHiddenWordArray = [];
-let currentHiddenWord = "";
+let currentHiddenTrickArray = [];
+let currentHiddenTrick = "";
 let gameOver = false;
 let wins = 0;
-let currentWord = "";
+let currentTrick = "";
 let guessesRemaining = 12;
 let lettersGuessed = "";
 let gameAlreadyStarted = false;
@@ -93,640 +80,171 @@ main();
 
 //Run all game functionality.
 function main() {
-
-    //Keypress initiates game activity
-    document.onkeyup = function (event) {
-
-        //See if this is the first keypress event
-        if (gameAlreadyStarted === false) {
-            //Initialize game data
-            init(wordsRandomOrder);
-        }
-
-        //This user input is not the first input
-        else {
-            processInput(event);
-
-            if (gameOver === true) {
-                //Determines if it was a win or a loss
-                resetGame();
-            }
-        }
+  //Keypress initiates game activity
+  document.onkeyup = function (event) {
+    //See if this is the first keypress event
+    if (gameAlreadyStarted === false) {
+      //Initialize game data
+      init(tricksRandomOrder);
     }
-}
 
-//Compares the new user input against past entries to find duplicates
-function isDuplicate(letter) {
-
-    for (let i = 0; i < lettersGuessed.length; i++) {
-
-        if (letter === lettersGuessed.charAt(i)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-//Resets all values for another game to start after determining whether or not it was a win.
-function resetGame() {
-
-    updateWinCount();
-    wordsRandomOrder = randomOrder(words);
-    guessesRemaining = 12;
-    gameAlreadyStarted = false;
-    lettersGuessed = "";
-    currentHiddenWord = "";
-    currentHiddenWordArray = [];
-    document.querySelector("#letters-guessed").innerHTML = lettersGuessed;
-    document.querySelector("#guesses-remaining").innerHTML = guessesRemaining;
-    document.querySelector("#wins").innerHTML = 0;
-    document.querySelector("#hidden-word").innerHTML = "";
-    document.querySelector("#letters-guessed").innerHTML = "None yet!"
-    gameOver = false;
-}
-
-//adjusts the win count according to win status
-function updateWinCount() {
-
-    console.log("updateWinCount " + guessesRemaining);
-    //Some guesses remaining, so it was a win!
-    if (guessesRemaining > 0) {
-
-        wins += 1;
-        console.log("wins " + wins);
-        document.querySelector("#wins").innerHTML = wins;
-        document.getElementById("headline").innerHTML = aux[1].text;
-        document.querySelector("#start-msg").innerHTML = "Good job!";
-    }
-    //...it was a loss
+    //This user input is not the first input
     else {
-        
-        wins = 0;
-        document.getElementById("headline").innerHTML = aux[2].text;
-        document.querySelector("#start-msg").innerHTML = "Try again!";
+      processInput(event);
+
+      if (gameOver === true) {
+        //Determines if it was a win or a loss
+        resetGame();
+      }
     }
-}
-
-//Compare input against the currentWord, if correct, update currentWordDashed, if guess is incorrect, update guessesRemaining and the lettersGuessed list. If there are no more guesses left, end game.
-function processInput(event) {
-
-    let input = event.key;
-    let keyInput = input.toLowerCase();
-
-    if (!isDuplicate(keyInput) && !isTheCorrectLetter(keyInput)) {
-
-        guessesRemaining--;
-
-        if (guessesRemaining === 11) {
-            console.log("processInput 11 " + guessesRemaining);
-            lettersGuessed = keyInput;
-        }
-        else if (guessesRemaining > 0) {
-            console.log("processInput >0 " + guessesRemaining);
-            lettersGuessed = lettersGuessed + ", " + keyInput;
-        }
-        else {
-            console.log("processInput == 0 " + guessesRemaining);
-            gameOver = true;
-        }
-    }
-    document.querySelector("#letters-guessed").innerHTML = lettersGuessed;
-    document.querySelector("#guesses-remaining").innerHTML = guessesRemaining;
-}
-
-//See if the current input matches any characters in the current word
-function isTheCorrectLetter(letter) {
-
-    let atLeastOneLetterChanged = false;
-
-    for (var i = 0; i < currentWord.length; i++) {
-
-        if (currentWord.charAt(i) === letter) {
-            currentHiddenWordArray[i] = letter;
-            atLeastOneLetterChanged = true;
-        }
-    }
-    //Pass the hidden word as it is at this point and check if the word was solved
-    currentHiddenWord = currentHiddenWordArray.toString();
-
-    //If at least one of the letters guessed was correct, display the current hidden word to the user and return true
-    if (atLeastOneLetterChanged) {
-
-        document.querySelector("#hidden-word").innerHTML = currentHiddenWord.replace(/,/g, ' ');
-        if (currentHiddenWord.replace(/,/g, '') === currentWord) { gameOver = true; }
-
-        return true;
-    }
-    return false;
-}
-
-//Normalizes user input, updates the start message and sets the game flag
-function updateStartChanges() {
-
-    document.getElementById("headline").innerHTML = aux[0].text;
-    document.querySelector("#start-msg").innerHTML = "Good Luck!";
-    gameAlreadyStarted = true;
+  };
 }
 
 //Initializing all game data
 function init(arr) {
+  updateStartChanges();
+  initCurrentAndDashedTrick(arr);
+}
 
-    updateStartChanges();
-    initCurrentAndDashedWord(arr);
+//Normalizes user input, updates the start message and sets the game flag
+function updateStartChanges() {
+  document.getElementById("headline").innerHTML = aux[0].text;
+  document.getElementById("start-msg").innerHTML = aux[0].msg;
+  console.log("aux[0].audio ", aux[0].audio);
+  aux[0].audio.play();
+  console.log("updateStartChanges()");
+  gameAlreadyStarted = true;
 }
 
 //Initializes the current word and the dashed form of it
-function initCurrentAndDashedWord(arr) {
+function initCurrentAndDashedTrick(arr) {
+  for (let i = 0; i < arr[currentIndex].trickName.length; i++) {
+    currentHiddenTrickArray.push("_");
+  }
 
-    for (let i = 0; i < arr[currentIndex].trickName.length; i++) {
-        currentHiddenWordArray.push("_");
+  currentTrick = arr[currentIndex].trickName;
+  currentHiddenTrick = currentHiddenTrickArray.join(" ");
+  console.log("initCUrrentAndDashedTruick()");
+  document.getElementById("headline-wins").innerHTML = "Wins";
+  document.getElementById("wins").innerHTML = wins;
+  document.getElementById("hidden-trick").innerHTML = currentHiddenTrick;
+  document.getElementById("number-of-guesses-remaining").innerHTML = "Number of Guesses Remaining";
+  document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+  document.getElementById("letters-already-guessed").innerHTML = "Letters Already Guessed";
+  document.getElementById("letters-guessed").innerHTML = "None yet!";
+  document.getElementById("game-graphic").src = tricks[currentIndex].trickVideo;
+}
+
+//Compares the new user input against past entries to find duplicates
+function isDuplicate(letter) {
+  for (let i = 0; i < lettersGuessed.length; i++) {
+    if (letter === lettersGuessed.charAt(i)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//Resets all values for another game to start after determining whether or not it was a win.
+function resetGame() {
+  updateWinCount();
+  tricksRandomOrder = randomOrder(tricks);
+  guessesRemaining = 12;
+  gameAlreadyStarted = false;
+  lettersGuessed = "";
+  currentHiddenTrick = "";
+  currentHiddenTrickArray = [];
+  document.getElementById("letters-guessed").innerHTML = lettersGuessed;
+  document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+  document.getElementById("letters-guessed").innerHTML = "None yet!";
+  gameOver = false;
+}
+
+//adjusts the win count according to win status
+function updateWinCount() {
+  console.log("updateWinCount " + guessesRemaining);
+  //Some guesses remaining, so it was a win!
+  if (guessesRemaining > 0) {
+    wins++;
+    console.log("wins " + wins);
+    console.log("tricks[currentIndex].trickText ", tricks[currentIndex].trickText);
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("headline").innerHTML = aux[1].text;
+    document.getElementById("hidden-trick").innerHTML = tricks[currentIndex].trickText;
+    document.getElementById("start-msg").innerHTML = "Good job!";
+    document.getElementById("game-graphic").src = "assets/images/victory.jpg";
+  }
+  //...it was a loss
+  else {
+    wins = 0;
+    document.getElementById("headline").innerHTML = aux[2].text;
+    document.getElementById("start-msg").innerHTML = "Try again!";
+    document.getElementById("game-graphic").src = "assets/images/crash.jpg";
+  }
+}
+
+//Compare input against the currentWord, if correct, update currentWordDashed, if guess is incorrect, update guessesRemaining and the lettersGuessed list. If there are no more guesses left, end game.
+function processInput(event) {
+  let input = event.key;
+  let keyInput = input.toLowerCase();
+
+  if (!isDuplicate(keyInput) && !isTheCorrectLetter(keyInput)) {
+    guessesRemaining--;
+
+    if (guessesRemaining === 11) {
+      console.log("processInput 11 " + guessesRemaining);
+      lettersGuessed = keyInput;
+    } else if (guessesRemaining > 0) {
+      console.log("processInput > 0 " + guessesRemaining);
+      lettersGuessed = lettersGuessed + ", " + keyInput;
+    } else {
+      console.log("processInput == 0 " + guessesRemaining);
+      gameOver = true;
+    }
+  }
+  document.getElementById("letters-guessed").innerHTML = lettersGuessed;
+  document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+}
+
+//See if the current input matches any characters in the current word
+function isTheCorrectLetter(letter) {
+  let atLeastOneLetterChanged = false;
+
+  for (var i = 0; i < currentTrick.length; i++) {
+    if (currentTrick.charAt(i) === letter) {
+      currentHiddenTrickArray[i] = letter;
+      atLeastOneLetterChanged = true;
+    }
+  }
+  //Pass the hidden word as it is at this point and check if the word was solved
+  currentHiddenTrick = currentHiddenTrickArray.toString();
+
+  //If at least one of the letters guessed was correct, display the current hidden word to the user and return true
+  if (atLeastOneLetterChanged) {
+    document.getElementById(
+      "hidden-trick"
+    ).innerHTML = currentHiddenTrick.replace(/,/g, " ");
+    if (currentHiddenTrick.replace(/,/g, "") === currentTrick) {
+      gameOver = true;
     }
 
-    currentWord = arr[currentIndex].trickName;
-    currentHiddenWord = currentHiddenWordArray.join(" ");
-    document.querySelector("#wins").innerHTML = wins;
-    document.querySelector("#hidden-word").innerHTML = currentHiddenWord;
-    document.querySelector("#guesses-remaining").innerHTML = guessesRemaining;
-    document.querySelector("#letters-guessed").innerHTML = "None yet!"
+    return true;
+  }
+  return false;
 }
 
 //Takes an array of objects and returns a randomly ordered object array.
 function randomOrder(arr) {
+  let currentIndex = arr.length;
+  let tempObj = [];
+  let rand = 0;
 
-    let currentIndex = arr.length;
-    let tempObj = [];
-    let rand = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        rand = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        tempObj = arr[currentIndex];
-        arr[currentIndex] = arr[rand];
-        arr[rand] = tempObj;
-    }
-    return arr;
+  for (let i = 0; i < arr.length; i++) {
+    rand = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    tempObj = arr[currentIndex];
+    arr[currentIndex] = arr[rand];
+    arr[rand] = tempObj;
+  }
+  return arr;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
